359cebc7fa97e9e019cb5850801b5b1d4cb2e172
Charlotte Mason <charlotte.nosam@gmail.com>
2013-07-31 22:17:11 +0100

    rewriting galtoons.py to have dictionaries

--- examples/bulgelessdisks.py
+++ examples/bulgelessdisks.py
@@ -8,8 +8,6 @@ import pylab as plt
 import numpy
 import os
 import toons
-import math
-from matplotlib.patches import Ellipse
  
 # ======================================================================
 # Read in the data
@@ -29,22 +27,31 @@ agnbolometricluminosity = bulgelessdisks_array[:,4]
 # ----------------------------------------------------------------------
 # Prepare the data
 
+# calculate u-r magnitude
+urcolour = magu - magr
+# bulge mass
+bulgemass = bulgetototal * totalmass
+# disk mass
+diskmass = totalmass - bulgemass
+
+# ----------------------------------------------------------------------
 # Instantiate the Galtoons class
+
 mytoons = toons.Galtoons()
 
 # Instantiate a Galtoons object:
 # PJM: how about something like the following initialisation instead?
-# mycoords = {'x':x, 'y':y, 'ra':ra, 'dec':dec} etc
-# mybulges = {'mass':bulgemass, 'colour':urcolour}
+
+#mycoords = {'x':totalmass, 'y':urcolour}
+#mybulges = {'size':bulgesize, 'mass':bulgemass, 'colour':urcolour}
+#mydisks = {'size':disksize, 'mass':diskmass, 'colour':urcolour}
+
 # mytoons = toons.Galtoons(bulges=mybulges,disks=None,halos=None)
 # Dictionaries are nice because you can reference them by the name of a parameter.
 # Its a choice whether you do all this parsing of the data outside the galtoons
 # class or inside...
 
-# calculate u-r magnitude
-urcolour = magu - magr
-# bulge mass
-bulgemass = bulgetototal * totalmass
+
 # ----------------------------------------------------------------------
 
 # Realize the galtoons!
@@ -63,27 +70,20 @@ bulgemass = bulgetototal * totalmass
 # Plot u-r vs stellar mass
 ax = plt.subplot(111)
 
-# Disk Mass
-diskmass = totalmass - bulgemass
-#normdiskmass = mytoons.normarea(diskmass)
-mytoons.plot_toons(totalmass, urcolour, 0.03*diskmass, "orange")
-
-# Bulge Mass
-#normbulgemass = mytoons.normarea(bulgemass)
-mytoons.plot_toons(totalmass, urcolour, 0.03*bulgemass, "red")
-#ax.scatter(totalmass, urcolour, s=areabulgemass, c='r', alpha=1)
+# Plot toons
+mytoons.plot_toons(totalmass, urcolour, diskmass, bulgemass, "cyan")
 
 # ----------------------------------------------------------------------
 # Make the plot nice:
-ax.set_xlim(9.0,12.3)
-ax.set_ylim(0.8,3.3)
+ax.set_xlim(9.4,11.0)
+ax.set_ylim(1.0,3.0)
 plt.xlabel(r'Stellar Mass (log M$_{\odot}$)')
 plt.ylabel('u-r colour (mag)')
 plt.title('Late-Type Galaxies')
 plt.grid(True)
 
 # Save the plot and tell user what it's called:
-savedfile = "testbulgelessdisks-disks&bulge.png"
+savedfile = "testbulgelessdisks-disks&bulge-realisticcolours.png"
 plt.savefig(savedfile)
 print "Plot saved as "+os.getcwd()+"/"+savedfile 
 plt.show()
--- toons/galtoons.py
+++ toons/galtoons.py
@@ -4,8 +4,6 @@ import pylab as plt
 import numpy as np
 import math
 from matplotlib.patches import Ellipse
- 
-# PJM: I think we will want to represent both bulges and disks as ellipses...
 
 # ======================================================================
 
@@ -44,23 +42,51 @@ class Galtoons(object):
 
 # Normalise data columns to make toons easier to see - 
 # 'area' will be the area of the toon
-    def normarea(self, vectordata):
+    def normdata(self, vectordata):
 
         unitarea = np.sqrt(np.sum(vectordata*vectordata))
-        area = vectordata/unitarea   
-        return area
+      #  area = vectordata/unitarea   
+        return unitarea
         
+
+           
+
 # ----------------------------------------------------------------------
+# CAM: I'm not sure how useful these dictionaries are... Is there a better
+# way to do the plotting so that I don't just repeat myself, some kind of loop?
 
 # Plot ellipse toons
-    def plot_toons(self, x, y, vectordata, colour):
-        PI = 3.14162
+    def plot_toons(self, x, y, diskdata, bulgedata, colour):
+        # define dictionaries for bulges and disks
+        mybulge = {'area':bulgedata, 'colour':colour, 'eccentricity':0.3}
+        mydisk = {'area':diskdata, 'colour':colour, 'eccentricity':0.8}
+        
+        # need to work out appropriate size for the toons
+        normarea = self.normdata(mydisk['area'] + mybulge['area'])
+        scale = 3 / normarea
+              
+        # define PI
+        PI = 3.14159265359
+        
+        # plot on the current axes
         ax = plt.gca()
-        for i in range(len(vectordata)):
-            a = 0.5 * math.sqrt(vectordata[i] / PI)
-            b = 4 * a
+        
+        # plot disks
+        squashdisk = math.sqrt(1 - mydisk['eccentricity']*mydisk['eccentricity'])     
+        for i in range(len(mydisk['area'])):
+            b = squashdisk * math.sqrt(scale * mydisk['area'][i] / PI)
+            a = squashdisk * b
             ax.add_patch(Ellipse((x[i], y[i]), a, b, 
                                 facecolor=colour, edgecolor="black", 
-                                alpha=0.5, angle=-30.0))
+                                alpha=0.5, angle=-70.0))
 
+
+        # plot bulges
+        squashbulge = math.sqrt(1 - mybulge['eccentricity']*mybulge['eccentricity'])     
+        for i in range(len(mybulge['area'])):
+            b = squashbulge * math.sqrt(scale * mybulge['area'][i] / PI)
+            a = squashbulge * b
+            ax.add_patch(Ellipse((x[i], y[i]), a, b, 
+                                facecolor="blue", edgecolor="black", 
+                                alpha=0.5, angle=-70.0))
     
